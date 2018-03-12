53a75834d00616f74a7956b539e26f8bc27899d6
Tasnuva Noor <noortasnuva@gmail.com>
2018-03-12 10:15:31 -0400

    first commit

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const Page = db.define('page', {
+    title: Sequelize.STRING,
+    urlTitle: Sequelize.STRING,
+    content: Sequelize.TEXT,
+    status: Sequelize.ENUM('open', 'closed')
+})
+
+const User = db.define('user', {
+    name: Sequelize.STRING,
+    email: Sequelize.STRING
+})
+
+module.exports = {
+    Page: Page,
+    User: User
+}
